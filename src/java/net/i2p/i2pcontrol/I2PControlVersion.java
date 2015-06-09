18da573825f980465066f6ece21a38772febaf5e
dev <dev@robertfoss.se>
2015-06-09 17:32:18 +0000

    Bumped version to 0.1.0.

--- src/java/net/i2p/i2pcontrol/I2PControlVersion.java
+++ src/java/net/i2p/i2pcontrol/I2PControlVersion.java
@@ -5,7 +5,7 @@ import java.util.Set;
 
 public class I2PControlVersion {
     /** The current version of I2PControl */
-    public final static String VERSION = "0.0.9";
+    public final static String VERSION = "0.1.0";
 
     /** The current version of the I2PControl API being primarily being implemented */
     public final static int API_VERSION = 1;
